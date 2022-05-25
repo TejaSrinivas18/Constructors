48270b2a2e95e5b3f382c7bd151f2cb69aa31554
TejaSrinivas18 <tejasrinivas19@gmail.com>
2022-05-25 22:54:27 +0530

    "first class"

---
+++ src/AddingStrings.java
@@ -0,0 +1,22 @@
+
+public class AddingStrings {
+	
+	String name1 ;
+	String name2 ;
+	
+	AddingStrings(String name1, String name2){
+		this.name1 = name1;
+		this.name2 = name2;
+	}
+	
+	void display() {
+		System.out.println(name1 + " " + name2);
+	}
+	
+	public static void main(String[] args) {
+		AddingStrings ad = new AddingStrings("Java", "Programming"); 
+		ad.display();
+		//System.out.println(ad.name1 + "" + ad.name2);
+		
+		}
+}
